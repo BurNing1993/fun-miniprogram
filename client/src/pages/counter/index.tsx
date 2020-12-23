d6359a2ddcd89f8e47994e4d18d0a5288a191d1f
Joey <786678257@qq.com>
2020-12-23 21:30:46 +0800

    feat:store

--- client/src/pages/counter/index.tsx
+++ client/src/pages/counter/index.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { View, Text, Button } from "@tarojs/components";
+import { add, minus } from "../../store/counter/actions";
 
-const Counter = ()=>{
-   return (
-       <>Counter</>
-   )
-}
+const Counter = () => {
+  const counter = useSelector<RootState>(state => state.counter.num);
+  const dispatch = useDispatch();
+  return (
+    <View>
+      <Text>{Number(counter)}</Text>
+      <Button onClick={() => dispatch(add())}>+</Button>
+      <Button onClick={() => dispatch(minus())}>-</Button>
+    </View>
+  );
+};
 export default Counter;
